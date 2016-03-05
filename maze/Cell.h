a12930bc3c12f22c80e4b04cbf64195f82a664db
Nick Mosher <nicholastmosher@gmail.com>
2016-03-05 17:34:06 -0500

    Fixed bad merge.

--- maze/Cell.h
+++ maze/Cell.h
@@ -26,40 +26,6 @@ public:
 	 * Constructs a cell with the given number of dimensions.
 	 */
 	Cell (int numDimensions);
-	
-	/**
-	 * Puts a wall at the given location. The locations are incremental over
-	 * the number of dimensions and the two possible directions for that
-	 * dimension.
-	 * 
-	 * For a cell with N dimensions, the wall indexes are:
-	 * 
-	 *	{0+, 0-, 1+, 1-, ..., N+, N-}
-	 *	 0	 1	 2	 3		  2N  2N+1
-	 */
-	void createWall (int wallIndex);
-	
-	/**
-	 * Removes a wall from the given location. The locations are incremental over
-	 * the number of dimensions and the two possible directions for that
-	 * dimension.
-	 * 
-	 * For a cell with N dimensions, the wall indexes are:
-	 * 
-	 *	{0+, 0-, 1+, 1-, ..., N+, N-}
-	 *	 0	 1	 2	 3		  2N  2N+1
-	 */
-	void deleteWall (int wallIndex);
-	
-	/**
-	 * Determines whether this cell has a wall (true) or opening (false) in the
-	 * given dimension on the given side.
-	 * 
-	 * @param dimension The dimension number to check walls for.
-	 * @param side The side (true/false corresponds to front/back) to check.
-	 */
-	bool isWall (int dimension, bool side);
-};
 
 	/**
 	 * Puts a wall at the given location. The locations are incremental over
