57bb023d79384b9f917f3e5356d673c79f1089ae
Kevin Kong <mxk7199@rit.edu>
2016-03-06 13:15:36 +0000

    completed connection method

--- maze/Generation.cpp
+++ maze/Generation.cpp
@@ -98,40 +98,35 @@ void generatePath(Maze *maze) {
 }
 
 //isAdjacent(Cell *cell);
-void knockWalls(std::vector<cellList> superset) {
-	int numSets = superset.size();
-	int i = 0;
-	int j = 0;
-	while (i < numSets) {
-		while (j < numSets) {
-			if (superset[i][superset[i].size() - 1]->isAdjacent(superset[j][0])) {
-				//TODO get cells to knock down walls
-				srand(time(NULL));
-				double threshold = (sqrt(superset.size()) / superset.size());
-				double rn = (rand() % 10) / 10; // generate between 0.1 to 1.0
+void randomlyConnect(cellList *cells, int dimension, int* dimensionSize) {
+	if (dimension == 1) {
+		for (int i = 0; i < cellList.size() - 1; ++i) {
+			srand(time(NULL));
+			double rn = rand % 5;
+			if (rn > 3) {
+				cell* leftCell = cells[i];
+				cell* rightCell = cells[i + 1];
 				
-				//TODO if random never triggers, have 1 join as a failsafe
-				if (rn > threshold) {
-					int dimensionOfWall = 0;
-					while (dimensionOfWall < superset[j][0]->getNumDimensions()) {
-						int cell1Coordinate = superset[i][superset[i].size() - 1]->getCoordinates()[dimensionOfWall];
-						int cell2Coordinate = superset[j][0]->getCoordinates()[dimensionOfWall];
-						if (cell1Coordinate - cell2Coordinate != 0) {
-							superset[i][superset[i].size() - 1]->deleteWall(2*dimensionOfWall + 1);
-							superset[j][0]->deleteWall(2*dimensionOfWall);
-							break;
-						}
-						++dimensionOfWall;
-					}
-					joinSets(&superset[i], &superset[j]);
-					superset.erase(superset.begin() + j);
-				}
-				else {
-					++j;
-				}
+				leftCell->deleteWall(dimension);
+				rightCell->deleteWall(dimension - 1);
+				rightCell->setEllerSet(leftCell->getEllerSet());
 			}
 		}
-		++i;
-		j = 0;
+	}
+	
+	else {
+		int offset = 1;
+		for (int i = 0; i < dimension; ++i) {
+			offset *= dimensionSize[i];
+		}
+		for (int i = 0; i < offset; ++i) {
+			if (cellList[i].isAdjacent(cellList[i + offset]) {
+				cell* leftCell = cells[i];
+				cell* rightCell = cells[i + offset];
+				
+				leftCell->deleteWall(dimension * 2 - 1);
+				rightCell->deleteWall(dimension * 2 - 2);
+			}
+		}
 	}
 }
