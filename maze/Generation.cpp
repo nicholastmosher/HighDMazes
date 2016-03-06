57343ce3f4af7f0018ffe53b4203a1e95d2d88a2
Kevin Kong <mxk7199@rit.edu>
2016-03-06 12:25:46 +0000

    changed order of code to fix logical bug

--- maze/Generation.cpp
+++ maze/Generation.cpp
@@ -112,8 +112,6 @@ void knockWalls(std::vector<cellList> superset) {
 				
 				//TODO if random never triggers, have 1 join as a failsafe
 				if (rn > threshold) {
-					joinSets(&superset[i], &superset[j]);
-					superset.erase(superset.begin() + j);
 					int dimensionOfWall = 0;
 					int cell1Coordinate = superset[i][superset[i].size() - 1]->getCoordinates()[dimensionOfWall];
 					int cell2Coordinate = superset[j][0]->getCoordinates()[dimensionOfWall];
@@ -125,6 +123,8 @@ void knockWalls(std::vector<cellList> superset) {
 						}
 						++dimensionOfWall;
 					}
+					joinSets(&superset[i], &superset[j]);
+					superset.erase(superset.begin() + j);
 				}
 				else {
 					++j;
